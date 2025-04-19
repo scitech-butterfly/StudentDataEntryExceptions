c99d7c9fea0b0ea4364d89950b75d94fbe31993f
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-19 12:14:24 +0530

    Method to search by PRN

--- StudentOperations.java
+++ StudentOperations.java
@@ -21,4 +21,14 @@ class StudentOperations {
     student.display();
    }
   }
+
+  // Search Student by PRN
+  public Student searchByPRN(long prn) {
+   for (Student student : students) {
+    if (student.getPRN() == prn) {
+     return student;
+    }
+   }
+   return null;
+  }
 }
