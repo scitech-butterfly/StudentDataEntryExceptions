4b356094fd8f3b4d7dcf3ef6be2eeef8e0c1bf1d
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-19 12:02:44 +0530

    Created parameterized constructor of Student

--- Student.java
+++ Student.java
@@ -1,1 +1,18 @@
+// Student.java
 
+class Student {
+  private String name;
+  private long prn;
+  private String branch;
+  private String batch;
+  private double cgpa;
+
+  // Parameterized Constructor
+  public Student(String name, long prn, String branch, String batch, double cgpa) {
+   this.name = name;
+   this.prn = prn;
+   this.branch = branch;
+   this.batch = batch;
+   this.cgpa = cgpa;
+  }
+ }
