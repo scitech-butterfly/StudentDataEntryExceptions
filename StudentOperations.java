592e7a4bfa75f90f084131a8519b2cdee5de1052
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-19 12:08:19 +0530

    Created ArrayList of Students

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,1 +1,11 @@
+// StudentOperations.java
 
+import java.util.*;
+
+class StudentOperations {
+  ArrayList<Student> students; // array list of data type Student (object) which is a combination of String and int
+
+  public StudentOperations() {
+   this.students = new ArrayList<>();
+  }
+}
